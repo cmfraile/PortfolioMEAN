bda82e576dacfc025e13853ac36e7dd55c8cb666
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-09-30 15:48:13 +0200

    cascaron hecho

---
+++ dist/models/server.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var Server = /** @class */ (function () {
+    function Server() {
+        this.app = (0, express_1.default)();
+        this.port = process.env.PORT || '8000';
+    }
+    Server.prototype.listen = function () {
+        var _this = this;
+        this.app.listen(this.port, function () {
+            console.log("Servidor corriendo en puerto " + _this.port);
+            console.log("tsc --watch && nodemon dist/app.js &&");
+        });
+    };
+    return Server;
+}());
+exports.default = Server;
+//# sourceMappingURL=server.js.map
